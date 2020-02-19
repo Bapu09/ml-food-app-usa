5fc90c35e97b05bce0c0fe0909a4709c3e312451
Girija Sankar Dash <girijasankar@google.com>
2020-02-19 14:46:30 +0530

    Added GitIgnore and logs to trace error line

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, g, render_template, url_for, redirect, flash, make_response, request
 from werkzeug.utils import secure_filename
 from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+import keras.backend.tensorflow_backend as tb
+
 import os
 
 UPLOAD_FOLDER = './uploads'
+MODEL_FOLDER = './models'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MODEL_FOLDER'] = MODEL_FOLDER
 
 @app.route('/')
 @app.route('/index')
@@ -16,36 +21,62 @@ def index():
 @app.route('/upload', methods = ['POST']) 
 def upload_image():
     if request.method == 'POST':  
-        f = request.files['file']
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        hdrf = request.files['file_hdr']
+        imgf = request.files['file_img']
+        hdr_filename = secure_filename(hdrf.filename);
+        img_filename = secure_filename(imgf.filename);
+        
+        hdrf.save(os.path.join(app.config['UPLOAD_FOLDER'], hdr_filename))
+        imgf.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
         
         
         #Now Let's load JSON
-        m = load_model('./models/fat.json', './models/fat.h5')
+        print("Model Loading");
         
-        img = envi.open(os.path.join(app.config['UPLOAD_FOLDER'], filename),'media/uploads/rawfile1.img')
+        if(os.path.exists(os.path.join(app.config['MODEL_FOLDER'], 'fat.json'))) :
+            print("JSON File Exists");
+        else:
+            print("JSON File Doesn't Exists");
+            
+        if(os.path.exists(os.path.join(app.config['MODEL_FOLDER'], 'fat.h5'))) :
+            print("H5 File Exists");
+        else:
+            print("H5 File Doesn't Exists");
+            
+            
+        m = load_model(os.path.join(app.config['MODEL_FOLDER'], 'fat.json'), os.path.join(app.config['MODEL_FOLDER'], 'fat.h5'))
+        print("Model Loaded");
+        tb._SYMBOLIC_SCOPE.value = True
+        img = envi.open(os.path.join(app.config['UPLOAD_FOLDER'], hdr_filename), os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        print("Image Loaded");
         img = img[:,:,:]
         img1D = img_to_array(img)
+        print("Image to Array converted");
         mydate=datetime.now()
     
         opt = Adam(lr=0.001)
         m.compile(loss='mean_squared_error', optimizer=opt,metrics=[rmse,r2])
+        print("Model Complied");
         result=m.predict(img1D)
-        
+        print("Model Predicted");
+        print("Result "+result)
         
         return render_template('success.html')
     
     
     
-""" Independent Function """
+
 def load_model(json,weights):
-	json_file = open(json, 'r')#Json architecture file
-	loaded_model_json = json_file.read()
-	json_file.close()
-	model = model_from_json(loaded_model_json)
-	model.load_weights(weights)#.h5 file
-	return model
+    print("Reading JSON File")
+    json_file = open(json, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    print("Reading JSON Complete")
+    model = model_from_json(loaded_model_json)
+    print("Loading Weight File")
+    model.load_weights(weights)#.h5 file
+    print("Weight File Loaded")
+    return model
 
 
 def rmse(y_true, y_pred):
@@ -56,14 +87,3 @@ def r2(y_true, y_pred):
     SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
     return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 
-def img_to_array(img,threshold=200):
-    img = img[:, :, :]
-    img_xy = np.mean(img, axis=2)
-    img_th = np.where(img_xy > threshold, 1, 0)
-
-    img_bs = img[img_th == 1]
-    np_img = img_bs
-    red_img = np.mean(np_img, 0)
-    red_img = np.expands_dims(red_img,2)
-
-    return red_img
